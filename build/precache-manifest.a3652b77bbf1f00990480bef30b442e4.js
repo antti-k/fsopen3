14cd4a4f33dda8823fcc50acea0e44f22627dc94
Antti Karvanen <antti.karvanen@exove.fi>
2019-07-11 14:38:41 +0300

    Serve build from express

---
+++ build/precache-manifest.a3652b77bbf1f00990480bef30b442e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3477b7fa6b0e8599d5945c3fafc9d7b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "676efe7d47f1b9930e1d",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "3d3a8f970ab47ff14dd3",
+    "url": "/static/js/2.05fc259b.chunk.js"
+  },
+  {
+    "revision": "676efe7d47f1b9930e1d",
+    "url": "/static/js/main.524c5271.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
