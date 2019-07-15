71044be75c28a4f17bf7a8836e8c2a75f19801ba
Antti Karvanen <antti.karvanen@exove.fi>
2019-07-15 13:18:16 +0300

    eslint

--- index.js
+++ index.js
@@ -13,86 +13,86 @@ app.use(cors())
 app.use(express.static('build'))
 
 app.get('/api/persons', (req, res) => {
-	Person.find({})
-		.then(persons => {
-			res.json(persons)
-		})
+  Person.find({})
+    .then(persons => {
+      res.json(persons)
+    })
 })
 
 app.get('/info', (req, res) => {
-	const currentDate = new Date().toDateString()
-	const currentTime = new Date().toTimeString()
-	Person.countDocuments({}).then(count => {
-		res.send(`Phonebook has info for ${count} people</br>${currentDate}${currentTime}`)
-	})
-		.catch(error => next(error))
+  const currentDate = new Date().toDateString()
+  const currentTime = new Date().toTimeString()
+  Person.countDocuments({}).then(count => {
+    res.send(`Phonebook has info for ${count} people</br>${currentDate}${currentTime}`)
+  })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-	Person.findById(req.params.id)
-		.then(person => {
-			if (person) {
-				res.json(person.toJSON())
-			} else {
-				res.status(404).end()
-			}
-		})
-		.catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-	Person.findByIdAndRemove(req.params.id)
-		.then(result => {
-			res.status(204).end()
-		})
-		.catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-	const { body } = req
-	const { name, number } = body
+  const { body } = req
+  const { name, number } = body
 
-	const person = {
-		name,
-		number,
-	}
+  const person = {
+    name,
+    number,
+  }
 
-	Person.findByIdAndUpdate(req.params.id, person, { new: true })
-		.then(updatedPerson => {
-			res.json(updatedPerson.toJSON())
-		})
-		.catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons/', (req, res, next) => {
-	const body = req.body
+  const body = req.body
 
-	if (!body.name || !body.number) {
-		return res.status(400).json({
-			error: 'Name or number missing'
-		})
-	}
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'Name or number missing'
+    })
+  }
 
-	const { name, number } = body
+  const { name, number } = body
 
-	Person.find({})
-		.then(persons => {
-			if (persons.some(person => person.name === name)) {
-				return res.status(400).json({
-					error: 'Name must be unique'
-				})
-			}
+  Person.find({})
+    .then(persons => {
+      if (persons.some(person => person.name === name)) {
+        return res.status(400).json({
+          error: 'Name must be unique'
+        })
+      }
 
-			const person = new Person({
-				name,
-				number
-			})
+      const person = new Person({
+        name,
+        number
+      })
 
-			person.save().then(savedPerson => {
-				res.json(savedPerson.toJSON())
-			})
-				.catch(error => next(error))
-		})
+      person.save().then(savedPerson => {
+        res.json(savedPerson.toJSON())
+      })
+        .catch(error => next(error))
+    })
 })
 
 const errorHandler = (error, request, response, next) => {
@@ -110,5 +110,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
